4060b5bf5d8c9f65be84e77af1f30dc4a9e8de5b
Yusuke <yusukefujiwara0806@gmail.com>
2026-06-17 09:22:52 +0900

    Update test expected message

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,4 +7,4 @@ def test_read_root():
     response = client.get("/")
     
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello, World!"}
+    assert response.json() == {"message": "FastAPI deployed from ECS!"}
